19e4854b7d542c657d329e2898ff8dec05bd9e92
sys1yagi <sylc.yagi@gmail.com>
2011-09-13 20:53:48 +0900

    とりあえずコミット。

--- puzzle/looper.bat
+++ puzzle/looper.bat
@@ -1,11 +1,2 @@
-call start.bat run 2 50 10000 150 true MD
-call start.bat run 2 100 10000 150 true MD
-call start.bat run 2 150 10000 150 true MD
-call start.bat run 2 180 15000 150 true MD
-call start.bat run 2 210 15000 999 true MD
-call start.bat run 2 160 25000 999 true MD
-call start.bat run 2 180 25000 999 true MD
-call start.bat run 2 210 25000 999 true MD
-call start.bat run 2 250 25000 999 true MD 
 call start.bat run 1 180 40000 999 true MD
-call start.bat run 1 160 40000 80 true MD
+
